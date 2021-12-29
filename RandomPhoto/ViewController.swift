37d5bd43fbb516317630e76553bbec7e6f5edf6d
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-12-29 22:59:50 +0530

    feat(image): show random image

--- RandomPhoto/ViewController.swift
+++ RandomPhoto/ViewController.swift
@@ -20,7 +20,18 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         view.backgroundColor = .systemPink
         view.addSubview(imageView)
-        imageView.frame = CGRect(x:0, y:0, width: 300, height: 300)
+        imageView.frame = CGRect(x:0, y:0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
         imageView.center = view.center
+        getRandomPhoto()
+    }
+    
+    func getRandomPhoto() {
+        
+        let urlString = "https://source.unsplash.com/random/3000x3000"
+        let url = URL(string: urlString)!
+        guard let data = try? Data(contentsOf: url) else {
+            return
+        }
+        imageView.image = UIImage(data:data)
     }
 }
