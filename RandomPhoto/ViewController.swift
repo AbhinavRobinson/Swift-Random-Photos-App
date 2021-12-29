cc65d5c8c9705e1cf51028176a56711fdde453c9
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-12-30 00:03:49 +0530

    feat(button): add button with image change action

--- RandomPhoto/ViewController.swift
+++ RandomPhoto/ViewController.swift
@@ -14,20 +14,44 @@ class ViewController: UIViewController {
         imageView.backgroundColor = .white
         return imageView
     }()
+    
+    private let button: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .systemGray5
+        button.setTitle("Change Picture", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         view.backgroundColor = .systemPink
         view.addSubview(imageView)
-        imageView.frame = CGRect(x:0, y:0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+        view.addSubview(button)
+        imageView.frame = CGRect(x:0,
+                                 y:0,
+                                 width: view.frame.size.width,
+                                 height: view.frame.size.height)
         imageView.center = view.center
+        getRandomPhoto()
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        button.frame = CGRect(x:25,
+                              y:view.frame.size.height-40-view.safeAreaInsets.bottom,
+                              width: view.frame.size.width-50,
+                              height: 50)
+        button.layer.cornerRadius = 10.0
+    }
+    
+    @objc func didTapButton() {
         getRandomPhoto()
     }
     
     func getRandomPhoto() {
-        
-        let urlString = "https://source.unsplash.com/random/3000x3000"
+        let urlString = "https://source.unsplash.com/random/800x1200"
         let url = URL(string: urlString)!
         guard let data = try? Data(contentsOf: url) else {
             return
